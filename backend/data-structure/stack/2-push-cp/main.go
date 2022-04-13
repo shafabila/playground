02ce6fdc8653e4e8a3e5a1aa22f934ba4cec8180
shafa buchori <shafabch@upi.edu>
2022-04-13 14:23:01 +0700

    commit

--- backend/data-structure/stack/2-push-cp/main.go
+++ backend/data-structure/stack/2-push-cp/main.go
@@ -10,12 +10,32 @@ var ErrStackOverflow = errors.New("stack overflow")
 
 type Stack struct {
 	// TODO: answer here
+	Top  int
+	Size int
+	Data []int
+	//
 }
 
 func NewStack(size int) Stack {
 	// TODO: answer here
+	return Stack{
+		Top:  -1,
+		Size: size,
+		Data: []int{},
+		//
+	}
+
 }
 
 func (s *Stack) Push(Elemen int) error {
 	// TODO: answer here
+	if len(s.Data) == s.Size {
+		return ErrStackOverflow
+	} else {
+		s.Top++
+		s.Data = append(s.Data, Elemen)
+		return nil
+		//
+	}
+
 }
